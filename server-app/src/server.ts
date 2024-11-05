9bf2fccdafdcca60a1cc146bcd7b4562abe4802e
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-05 18:58:33 +0530

    chore: Refactor server URL and CORS origin handling in server configuration

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -7,22 +7,19 @@ import shortUrl from "./routes/shortUrl";
 dotenv.config();
 connectDb();
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://url-shortener-one-mauve.vercel.app",
-    ],
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
 
-app.use("/api/", shortUrl);
+app.use("/api/shorturl", shortUrl);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
